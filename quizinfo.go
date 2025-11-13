537fa513f420dd69b8176d8a4e4caabea86fda12
Khalil-trb <khalil.trabelsi@ynov.com>
2025-11-13 11:49:41 +0100

    Ajout des questions quiz info

--- quizinfo.go
+++ quizinfo.go
@@ -6,7 +6,7 @@ func StartQuizInfo() {
 	fmt.Println("=== Quiz Informatique ===")
 	fmt.Println("=== Quiz Informatique Générale ===")
 
-	totalQuestions := 3
+	totalQuestions := 10
 	score := 0
 	// Question 1
 	fmt.Println("Quel est le système d’exploitation libre le plus utilisé ?")
@@ -32,6 +32,62 @@ func StartQuizInfo() {
 	if CheckAnswer(1) {
 		score++
 	}
+	// Question 4
+	fmt.Println("Quel language de programmation est principalement utilisé pour le développement d'applications Android?")
+	fmt.Println("1. Swift")
+	fmt.Println("2. Java")
+	fmt.Println("3. Python")
+	if CheckAnswer(2) {
+		score++
+	}
+	// Question 5
+	fmt.Println("Que signifie CPU?")
+	fmt.Println("1. Central Processing Unit")
+	fmt.Println("2. Computer Power Unit")
+	fmt.Println("3. Central Program Unit")
+	if CheckAnswer(1) {
+		score++
+	}
+	// Question 6
+	fmt.Println("Quel protocole est utilisé pour sécuriser les communications sur Internet?")
+	fmt.Println("1. HTTP")
+	fmt.Println("2. FTP")
+	fmt.Println("3. HTTPS")
+	if CheckAnswer(3) {
+		score++
+	}
+	// Question 7
+	fmt.Println("Quel type de mémoire est volatile, c'est a dire qu'elle perd ses données lorsqu'on éteint l'ordinateur?")
+	fmt.Println("1. ROM")
+	fmt.Println("2. RAM")
+	fmt.Println("3. HDD")
+	if CheckAnswer(2) {
+		score++
+	}
+	// Question 8
+	fmt.Println("Que signifie SQL ?")
+	fmt.Println("1. Structured Query Language")
+	fmt.Println("2. Simple Query Language")
+	fmt.Println("3. Sequential Question Logic")
+	if CheckAnswer(1) {
+		score++
+	}
+	// Question 9
+	fmt.Println("Quelle structure de données utilise le principe LIFO (Last In, First Out)?")
+	fmt.Println("1. File d'attente")
+	fmt.Println("2. pile")
+	fmt.Println("3. Liste chainée")
+	if CheckAnswer(2) {
+		score++
+	}
+	// Question 10
+	fmt.Println("Quel est le role principal d'un pare-feu dans les réseaux informatiques ?")
+	fmt.Println("1. Windows")
+	fmt.Println("2. macOS")
+	fmt.Println("3. Linux")
+	if CheckAnswer(2) {
+	}
+	score++
 	CalculateScore(score, totalQuestions)
 }
 
@@ -52,9 +108,9 @@ func CheckAnswer(correctAnswer int) bool {
 func CalculateScore(score int, total int) {
 	fmt.Printf("Tu as %d/%d bonnes réponses.\n", score, total)
 
-	if score <= 1 {
+	if score <= 4 {
 		fmt.Println("🔰 Niveau : Apprenti codeur")
-	} else if score == 2 {
+	} else if score == 5 {
 		fmt.Println("💻 Niveau : Développeur en progression")
 	} else {
 		fmt.Println("🤯 Niveau : Cyber Mastermind")
