dd904e066808f6ed9ffa741ae5f89634d4809cdd
Khalil-trb <khalil.trabelsi@ynov.com>
2025-11-17 10:08:25 +0100

    creation des questionnaires

--- quizAIdata.go
+++ quizAIdata.go
@@ -3,6 +3,91 @@ package main
 import "fmt"
 
 func StartQuizIAData() {
-	fmt.Println("=== Quiz Informatique ===")
-	// plus tard : poser les questions ici
-}
+	fmt.Println("――――― Quiz IA et Data ―――――")
+	
+	
+	totalQuestions := 10
+	score := 0
+
+	// Question 1
+	fmt.Println("Qu'est ce qu'un modèle d'intelligence artificielle ?")
+	fmt.Println("1. Un programme capable d'apprednre a partir de données")
+	fmt.Println("2. Un robot physique autonome")
+	fmt.Println("3. Un systeme qui n'utilise aucune données")
+	if CheckAnswer(1) {
+		score++
+	}
+	// Question 2
+	fmt.Println("Que signifie machine learning ?")
+	fmt.Println("1. L'utilisation d'ordinateurs sans algorithmes")
+	fmt.Println("2. L'apprentissage automatique a partir de données")
+	fmt.Println("3. La création d'images a partir de texte")
+	if CheckAnswer(2) {
+		score++
+	}
+	// Question 3
+	fmt.Println("Quel est l'objectif principal du Big Data ?")
+	fmt.Println("1. Réduire la quantité de données disponible")
+	fmt.Println("2. Gérer et analyser de grands volumes de données")
+	fmt.Println("3.Construire des ordinateurs plus petits")
+	if CheckAnswer(2) {
+		score++
+	}
+	// Question 4
+	fmt.Println("Que représente uen feature (variable) en data science ?")
+	fmt.Println("1. Une donnée d'entrée utilisée par le modèle")
+	fmt.Println("2. Le code source complet du programme")
+	fmt.Println("3. La réponse finale du modèle")
+	if CheckAnswer(1) {
+		score++
+	}
+	// Question 5
+	fmt.Println("A quoi sert un algorithme de classification?")
+	fmt.Println("1. Deviner le prochaion nombre d'une suite")
+	fmt.Println("2. Prédire une categorie ou une classe")
+	fmt.Println("3. Améliorer la vitesse du processeur")
+	if CheckAnswer(2) {
+		score++
+	}
+	// Question 6
+	fmt.Println("Qu'est ce qu'un Dataset?")
+	fmt.Println("1. Un type de réseau informatique")
+	fmt.Println("2. Un ensemble organisé de données")
+	fmt.Println("3. Un modèle d'IA pré-entrainé")
+	if CheckAnswer(2) {
+		score++
+	}
+	// Question 7
+	fmt.Println(" Que fait un réseau de neruones artificels?")
+	fmt.Println("1. Imite de manière simplifiée le fonctionnement du cerveay humain")
+	fmt.Println("2. Stocke des fichiers dans le cloud")
+	fmt.Println("3. Crée des circuits électroniques")
+	if CheckAnswer(1) {
+		score++
+	}
+	// Question 8
+	fmt.Println("Quand parle-t-on d'overfitting (surapprentissage)?")
+	fmt.Println("1. Le modèle oublie toutes les données")
+	fmt.Println("2. Le modèle devient trop simple")
+	fmt.Println("3. Le modèle apprend trop bien les données d'entrainement et généralise mal")
+	if CheckAnswer(3) {
+		score++
+	}
+	// Question 9
+	fmt.Println("Quel language est le plus répandu pour l'IA?")
+	fmt.Println("1. html")
+	fmt.Println("2. Bash")
+	fmt.Println("3. Python")
+	if CheckAnswer(3) {
+		score++
+	}
+	// Question 10
+	fmt.Println(" A quoi sert une base de données ?")
+	fmt.Println("1. A exécuter du code Go")
+	fmt.Println("2. A stocker et organiser des informations")
+	fmt.Println("3. A produire des images")
+	if CheckAnswer(2) {
+	}
+	score++
+	CalculateScore(score, totalQuestions)
+ }
--- quizcyber.go
+++ quizcyber.go
@@ -4,5 +4,91 @@ import "fmt"
 
 func StartQuizCyber() {
 	fmt.Println("=== Quiz Cybersecurity ===")
-	// plus tard : poser les questions ici
-}
+	
+	
+	totalQuestions := 10
+	score := 0
+
+	// Question 1
+	fmt.Println("Quel est le meilleur moyen de créer un mot de passe sécurisé ?")
+	fmt.Println("1. Utiliser sa date de naissance ")
+	fmt.Println("2. Utiliser un mot unique facile à retenir")
+	fmt.Println("3. Combiner lettres,chiffres et symboles")
+	if CheckAnswer(3) {
+		score++
+	}
+	// Question 2
+	fmt.Println("Quel type d'attaque consiste a tromper un utilisateur via un faux e-mail ?")
+	fmt.Println("1. Phishing")
+	fmt.Println("2. DDoS")
+	fmt.Println("3. Ransomware")
+	if CheckAnswer(1) {
+		score++
+	}
+	// Question 3
+	fmt.Println("Quel est le role d'un antivirus ?")
+	fmt.Println("1. Booster les performances du PC")
+	fmt.Println("2. Détecter et supprimer les logiciels malveillants")
+	fmt.Println("3. Bloquer les e-mails")
+	if CheckAnswer(2) {
+		score++
+	}
+	// Question 4
+	fmt.Println("Quel est protocole est le plus sécurisé pour naviguer sur un site web  ?")
+	fmt.Println("1. HTTP")
+	fmt.Println("2. FTP")
+	fmt.Println("3. HTTPS")
+	if CheckAnswer(3) {
+		score++
+	}
+	// Question 5
+	fmt.Println("Quel support est le plus vulnerable a la perte ou au vol de données?")
+	fmt.Println("1. Clé USB non chiffrée")
+	fmt.Println("2. Disque dur externe chiffré")
+	fmt.Println("3. Cloud sécurisé")
+	if CheckAnswer(1) {
+		score++
+	}
+	// Question 6
+	fmt.Println("Quel terme décrit un logiciel qui demande une rancon pour lib&rer les données?")
+	fmt.Println("1. Spyware")
+	fmt.Println("2. Ransomware")
+	fmt.Println("3. Adware")
+	if CheckAnswer(2) {
+		score++
+	}
+	// Question 7
+	fmt.Println(" Quelle action renforce le plus la sécurité d'un compte en ligne?")
+	fmt.Println("1. Utiliser le meme mot de passe partout")
+	fmt.Println("2. Activer l'authentification a deux facteur")
+	fmt.Println("3. Partager son mot de passe avec un collègue de confiance")
+	if CheckAnswer(2) {
+		score++
+	}
+	// Question 8
+	fmt.Println("Quel comportement est risqué en cybersecurité?")
+	fmt.Println("1. Mettre régulièrement a jour ses logiciels")
+	fmt.Println("2. Cliquer sur un lien recu par un inconnu")
+	fmt.Println("3. Sauvegarder ses données")
+	if CheckAnswer(2) {
+		score++
+	}
+	// Question 9
+	fmt.Println(" l est le principal risque lié au WI-Fi public non sécurisé?")
+	fmt.Println("1. Une consommation excessive de batterie")
+	fmt.Println("2. L'interception de vos données")
+	fmt.Println("3. Une connexion plus lente")
+	if CheckAnswer(2) {
+		score++
+	}
+	// Question 10
+	fmt.Println(" Quel outil permet de chiffrer des fichiers pour les rendres illisible sans clé ?")
+	fmt.Println("1. Un logiciel de chiffrement")
+	fmt.Println("2. Un lecteur multimedia")
+	fmt.Println("3. Un navigateur web")
+	if CheckAnswer(1) {
+	}
+	score++
+	CalculateScore(score, totalQuestions)
+ }
+
